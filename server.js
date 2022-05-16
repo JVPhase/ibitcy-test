0442b0548b63eebedec4f3f6a318a52ff5cbd90d
jvphase <john.vasiliev@gmail.com>
2022-05-16 05:09:49 +0300

    remove lowdb

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 import express from 'express';
-import { Low, JSONFile } from 'lowdb';
-import { join, dirname } from 'path';
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import bodyParser from 'body-parser';
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const file = join(__dirname, 'db.json');
-const adapter = new JSONFile(file);
-const db = new Low(adapter);
-await db.read();
 let app = express();
 const srcPath = __dirname;
 app.use(express.static(path.join(srcPath, 'build')));
@@ -20,30 +15,26 @@ let defaultDeals = [
   { id: 0, date: 1652621670000, value: 200 },
   { id: 1, date: 1652621671000, value: 68 },
 ];
-db.data.deals = [];
-db.write();
+let deals = [];
 defaultDeals.forEach((deal) => {
-  db.data.deals.push(deal);
-  db.write();
+  deals.push(deal);
 });
 
 app.get('/deals/:page', (req, res) => {
-  const deals = [...db.data.deals].reverse();
-  const filtered = deals.splice(req.params.page * 10, 10);
+  const dealsRev = [...deals].reverse();
+  const filtered = dealsRev.splice(req.params.page * 10, 10);
   res.send(filtered);
 });
 
 app.post('/new', urlencodedParser, (req, res) => {
-  db.data.deals.push(req.body);
-  db.write();
+  deals.push(req.body);
   res.sendStatus(200);
 });
 
 app.delete('/delete/:id', urlencodedParser, (req, res) => {
-  for (let i = 0; i < db.data.deals.length; i++) {
-    if (db.data.deals[i].id === +req.params.id) {
-      db.data.deals.splice(i, 1);
-      db.write();
+  for (let i = 0; i < deals.length; i++) {
+    if (deals[i].id === +req.params.id) {
+      deals.splice(i, 1);
       break;
     }
   }
